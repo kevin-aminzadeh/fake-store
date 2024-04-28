27aed7ef68fdd2d4701102cedebd8837daec87de
Kevin Aminzadeh <kevin_aminzadeh@live.com.au>
2024-04-28 18:39:58 +0930

    MILE-01 fix: attempt EAS Update index route fix using Expo Router Stack component

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
 import "expo-dev-client";
 import "../styles/global.css";
 
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
@@ -20,7 +20,11 @@ function AppLayout() {
         <StatusBar style="dark" />
         <GestureHandlerRootView>
           <View className="bg-light flex flex-col w-full h-full justify-center">
-            <Slot />
+            <Stack
+              screenOptions={{
+                headerShown: false,
+              }}
+            />
           </View>
         </GestureHandlerRootView>
       </RootSiblingParent>
