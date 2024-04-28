a8a99afb99e6395e82cf57e57c546b9460cd8625
Kevin Aminzadeh <kevin_aminzadeh@live.com.au>
2024-04-28 17:59:41 +0930

    MILE-01 refactor: update TitleBar style and linking behavior

--- src/components/core/title-bar.tsx
+++ src/components/core/title-bar.tsx
@@ -1,6 +1,6 @@
-import { Octicons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 import * as Haptics from "expo-haptics";
-import { Link } from "expo-router";
+import { router } from "expo-router";
 import { Pressable, View } from "react-native";
 
 import Heading from "./heading";
@@ -11,6 +11,7 @@ type TitleBarProps = {
   secondaryNav?: React.ReactNode;
   showBackButton?: boolean;
   onBackButtonPress?: () => void;
+  variant?: "dark" | "light";
 };
 
 function TitleBar({
@@ -19,45 +20,57 @@ function TitleBar({
   secondaryNav,
   showBackButton = false,
   onBackButtonPress = () => {},
+  variant = "light",
 }: TitleBarProps) {
   return (
-    <View className="w-full flex flex-col justify-center items-start py-12 pr-8 gap-3">
+    <View className="w-full flex flex-col justify-center items-start pr-8 gap-3">
       {subtitle && (
         <Heading
           className="pl-8"
           text={subtitle}
           variant="subtitle"
           size="xl"
-          color="neutral"
+          color={variant === "dark" ? "dark" : "light"}
         />
       )}
 
       <View className="flex flex-row items-top justify-between w-full">
         {showBackButton ? (
-          <Link href="/" asChild>
-            <Pressable
-              onPress={() => {
-                Haptics.impactAsync(
-                  Haptics.ImpactFeedbackStyle.Light,
-                );
+          <Pressable
+            onPress={() => {
+              Haptics.impactAsync(
+                Haptics.ImpactFeedbackStyle.Light,
+              );
+              router.back();
 
-                onBackButtonPress();
-              }}
-              className="flex flex-row items-center gap-6 pl-8"
-            >
-              <Octicons
-                name="chevron-left"
-                size={24}
-                color="white"
+              onBackButtonPress();
+            }}
+            className="flex flex-row items-center gap-6 pl-8"
+          >
+            <Ionicons
+              name="chevron-back"
+              size={24}
+              color={
+                variant === "dark" ? "#111111" : "#ffffff"
+              }
+            />
+            {title && (
+              <Heading
+                text={title}
+                size="4xl"
+                className="capitalize"
+                color={
+                  variant === "dark" ? "dark" : "light"
+                }
               />
-              <Heading text={title} size="4xl" />
-            </Pressable>
-          </Link>
+            )}
+          </Pressable>
         ) : (
           <Heading
             text={title}
             size="4xl"
-            className="pl-8"
+            className="pl-8 capitalize"
+            color={variant === "dark" ? "dark" : "light"}
           />
         )}
 
